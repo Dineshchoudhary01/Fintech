4b1b780a04a7c26ff54d759856ac7283c45ccf31
dc967 <dc3879643@gmail.com>
2026-08-23 14:54:41 +0530

    Transcation model complete

---
+++ backend/src/models/Transaction.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const TransactionSchema = new mongoose.Schema({
+
+  user : {
+    type: mongoose.Schema.Types.ObjectId,
+    ref : 'User',
+    required: true  
+  },
+
+  amount : {
+    type: Number,
+    required: true
+  },
+
+  type : {
+    type : String,
+    enum: ['income', 'expense'],
+    required: true
+  },
+
+  description: {
+    type: String,
+    required: true
+  },
+
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+
+  categorySource: {
+    type: String,
+    enum: ['manual','ai'],
+    required: true
+  },
+
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true
+  },
+
+  paymentMethod: {
+    type: String
+  }
+
+
+},{timestamps: true});
+
+TransactionSchema.index({ user: 1, date: -1});
+
+module.exports = mongoose.model('Transaction', TransactionSchema)
